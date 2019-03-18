c06de886ff3568aa98724d713801a02a27cb4462
Brian Mayo <ioprotium@gmail.com>
2019-03-17 22:46:04 -0300

    Fixing specs

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,3 +1,19 @@
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      head :not_found
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      head :unprocessable_entity
+    end
+  end
+end
+
 class ApplicationController < ActionController::API
+  include ExceptionHandler
+  
   protect_from_forgery with: :null_session
 end
