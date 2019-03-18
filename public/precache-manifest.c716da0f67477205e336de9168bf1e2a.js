eb71117260262c1035f6147589190746d2702c91
Brian Mayo <ioprotium@gmail.com>
2019-03-18 03:21:16 -0300

    App the missing files for public folder

---
+++ public/precache-manifest.c716da0f67477205e336de9168bf1e2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f0e11ca703789d3d199",
+    "url": "/static/css/main.0536efce.chunk.css"
+  },
+  {
+    "revision": "6f0e11ca703789d3d199",
+    "url": "/static/js/main.f8981a3e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6da6b355a139c915eff",
+    "url": "/static/js/2.454e3fdb.chunk.js"
+  },
+  {
+    "revision": "9032fb1be1fcd57e8713de33f034f5cd",
+    "url": "/index.html"
+  }
+];
